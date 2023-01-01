170523978ba9fcf2eec8ea9ff2fcf4ff0e605cdb
Michael <94321938+bigshmike@users.noreply.github.com>
2023-01-01 08:19:17 -0500

    Delete CPS278-final-mbrown99 directory

--- CPS278-final-mbrown99/src/main/java/edu/wccnet/mbrown99/finProj/dao/CustomerMovieDAO.java
+++
@@ -1,9 +0,0 @@
-package edu.wccnet.mbrown99.finProj.dao;
-
-import java.util.List;
-
-import edu.wccnet.mbrown99.finProj.entity.CustomerMovie;
-
-public interface CustomerMovieDAO {
-	public List<CustomerMovie> getRentalHistory(int id);
-}
